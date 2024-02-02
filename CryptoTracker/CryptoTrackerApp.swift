defdd8c30baf58bde83c3d4185b690e21ccc4761
Berkay Özdemir <berkay.ozdemir@optiyol.com>
2024-02-02 16:59:22 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Berkay on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
